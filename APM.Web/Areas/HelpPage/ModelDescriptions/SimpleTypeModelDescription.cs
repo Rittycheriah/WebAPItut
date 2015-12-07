aace9497f4aba54181d0da708f0f0d620ea11ca7
unknown <rittycheriah@gmail.com>
2015-12-06 19:52:34 -0600

    initial commit

---
+++ APM.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APM.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ APM.Web/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APM.Web.Models;
+
+namespace APM.Web.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        public IEnumerable<Product> Get()
+        {
+            var ProductRepository = new ProductRepository();
+            return ProductRepository.Retrieve();
+        }
+
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
